d6aba7bcd1c2f12495089ce70b9bd97825177144
Naveen Bugalia <naveenbugalia512@gmail.com>
2025-10-23 22:47:07 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,16 +33,19 @@ const Header = () => {
     navigate("/login");
   }, [dispatch, navigate]);
 
-  const fetchUserData = useCallback(async (id) => {
-    try {
-      const fetchedUser = await fetchUser(id);
-      setProfileUser(fetchedUser);
-    } catch (error) {
-      console.error("Failed to fetch user data:", error);
-    } finally {
-      setLoading(false);
-    }
-  }, [user]);
+  const fetchUserData = useCallback(
+    async (id) => {
+      try {
+        const fetchedUser = await fetchUser(id);
+        setProfileUser(fetchedUser);
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [user]
+  );
 
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -126,7 +129,7 @@ const Header = () => {
             <div className="flex justify-center items-center bg-gray-800 rounded-full p-0">
               {profileUser?.avatar ? (
                 <img
-                  src={`${API_BARE_BASE_URL}${profileUser.avatar}`}
+                  src={profileUser.avatar}
                   alt="Profile"
                   className="w-14 h-14 object-cover rounded-full"
                 />
@@ -159,8 +162,7 @@ const Header = () => {
       </div>
     );
 
-
-    if(!navigator.onLine) return;
+  if (!navigator.onLine) return;
 
   // if (loading) {
   //   return (
@@ -172,51 +174,56 @@ const Header = () => {
 
   return (
     <div>
-      {navigator.onLine && !loading && <IconContext.Provider value={{ className: "react-icons" }}>
-        <nav className="bg-white shadow-sm">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-              {/* Left side (logo and nav items) */}
-              <div className="flex">
-                <Link to={`/clients`} className="flex-shrink-0 flex items-center">
-                  <img className="h-10 w-auto" src={Logo} alt="Logo" />
-                </Link>
-                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  {renderNavItems()}
-                </div>
-              </div>
-
-              {/* Right side (profile and mobile menu) */}
-              <div className="flex items-center gap-2">
-                <div className="flex-shrink-0 relative" ref={profileMenuRef}>
-                  {renderProfileButton()}
-                  {renderProfileMenu()}
-                </div>
-
-                {/* Mobile menu button */}
-                <div className="-mr-2 flex items-center sm:hidden">
-                  <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500"
+      {navigator.onLine && !loading && (
+        <IconContext.Provider value={{ className: "react-icons" }}>
+          <nav className="bg-white shadow-sm">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex justify-between h-16">
+                {/* Left side (logo and nav items) */}
+                <div className="flex">
+                  <Link
+                    to={`/clients`}
+                    className="flex-shrink-0 flex items-center"
                   >
-                    <span className="sr-only">Open main menu</span>
-                    {isOpen ? (
-                      <FaTimes className="h-6 w-6" />
-                    ) : (
-                      <FaBars className="h-6 w-6" />
-                    )}
-                  </button>
+                    <img className="h-10 w-auto" src={Logo} alt="Logo" />
+                  </Link>
+                  <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                    {renderNavItems()}
+                  </div>
+                </div>
+
+                {/* Right side (profile and mobile menu) */}
+                <div className="flex items-center gap-2">
+                  <div className="flex-shrink-0 relative" ref={profileMenuRef}>
+                    {renderProfileButton()}
+                    {renderProfileMenu()}
+                  </div>
+
+                  {/* Mobile menu button */}
+                  <div className="-mr-2 flex items-center sm:hidden">
+                    <button
+                      onClick={() => setIsOpen(!isOpen)}
+                      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500"
+                    >
+                      <span className="sr-only">Open main menu</span>
+                      {isOpen ? (
+                        <FaTimes className="h-6 w-6" />
+                      ) : (
+                        <FaBars className="h-6 w-6" />
+                      )}
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          {/* Mobile menu */}
-          <div className={`${isOpen ? "block" : "hidden"} sm:hidden`}>
-            <div className="pt-2 pb-3 space-y-1">{renderNavItems(true)}</div>
-          </div>
-        </nav>
-      </IconContext.Provider>}
+            {/* Mobile menu */}
+            <div className={`${isOpen ? "block" : "hidden"} sm:hidden`}>
+              <div className="pt-2 pb-3 space-y-1">{renderNavItems(true)}</div>
+            </div>
+          </nav>
+        </IconContext.Provider>
+      )}
     </div>
   );
 };
